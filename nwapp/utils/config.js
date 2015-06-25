630b8175a2f1f6ef46abba2acdc112604b6240cd
Andy Trevorah <andy@troupe.co>
2015-06-25 15:06:51 +0100

    removed developer welcome screen

--- nwapp/utils/config.js
+++ nwapp/utils/config.js
@@ -1,8 +1,6 @@
 /* global OAUTH: true */
 'use strict';
 
-var gui = window.require('nw.gui');
-
 /*
  * The `OAUTH` object is defined in a nwsnapshot binary that is only
  * added during the production build. Used for obfuscation.
@@ -17,7 +15,6 @@ var key = (process.env.OAUTH_KEY || typeof OAUTH !== 'undefined' && OAUTH.key ||
 var secret = (process.env.OAUTH_SECRET || typeof OAUTH !== 'undefined' && OAUTH.secret || '').trim();
 
 if (!key || !secret) {
-  /*var win = */gui.Window.get(window.open('../welcome.html')); // WC: instructions for developers
   throw new Error('You must provide a key. Keys can be obtained from https://developer.gitter.im');
 }
 
