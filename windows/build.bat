24549aec35e0264a4c22f62b824672a743488a13
Eric Eastwood <contact@ericeastwood.com>
2016-02-08 05:59:23 -0600

    Start refactor windows installer build to node script

--- windows/build.bat
+++ windows/build.bat
@@ -1,8 +1,8 @@
 if "%1"=="" (
   echo "please provide a version x.x.x"
 ) else (
-  "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f "E:\certificates\troupe-cert.pfx" "E:\opt\Gitter\win32\Gitter.exe"
-  "C:\Program Files\Inno Setup 5\ISCC.exe" "E:\windows\gitter.iss"
-  "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f "E:\certificates\troupe-cert.pfx" "E:\artefacts\GitterSetup*"
-  rename "E:\artefacts\GitterSetup.exe" "GitterSetup-%1%.exe"
+  "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\Bin\signtool.exe" sign /f "Z:\certificates\troupe-cert.pfx" "Z:\opt\Gitter\win32\Gitter.exe"
+  "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" "Z:\windows\gitter.iss"
+  "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\Bin\signtool.exe" sign /f "Z:\certificates\troupe-cert.pfx" "Z:\artefacts\GitterSetup*"
+  rename "Z:\artefacts\GitterSetup.exe" "GitterSetup-%1%.exe"
 )
---
+++ windows/build.js
@@ -0,0 +1,42 @@
+
+
+var argv = require('yargs').argv;
+var Promise = require('bluebird');
+
+var exec = require('child_process').exec;
+
+
+var nwappManifest = require('./nwapp/package.json');
+var appVersion = nwappManifest.version;
+
+
+
+var projectPath = argv.projectPath || 'E:\\';
+
+var commandList = [
+	'"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\Bin\signtool.exe" sign /f "Z:\certificates\troupe-cert.pfx" "Z:\opt\Gitter\win32\Gitter.exe"',
+	'"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" "E:\windows\gitter.iss"',
+	'"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\Bin\signtool.exe" sign /f "Z:\certificates\troupe-cert.pfx" "Z:\artefacts\GitterSetup*"',
+	'rename "Z:\artefacts\GitterSetup.exe" "GitterSetup-' + appVersion + '.exe"',
+];
+
+
+var commandRunner = function(command) {
+	return new Promise(function(resolve, reject) {
+		var child = exec(command, function(err, stdout, stderr) {
+			//console.log('stdout', stdout, 'e', err, 'stderr', stderr);
+			resolve({
+				stdout: stdout,
+				stderr: stderr,
+				error: err
+			});
+		});
+	});
+};
+
+
+
+commandList.map(function(command) {
+	console.log(command);
+	//return commandRunner(command);
+});
